7c8d443aee869b0c843ab4ca22eabf477230c804
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-01-31 00:13:10 -0800

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Vladimir on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
